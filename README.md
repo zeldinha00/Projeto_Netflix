f38d49aa10445c370bb9580b5703085ce75dab88
Roger Oliveira Silva <zeldinhamtx@gmail.com>
2022-04-10 22:15:11 -0300

    :package: Nova funcionalidade

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Projeto_Netflix
-Clone Netflix
+# Clone site Netflix
+
+ :point_right: Clone do site Netflix responsivel para seus projetos.
+
+## Tarefas
+
+:point_right: O controle das tarefas desse projeto será realizado no GitHub.
+
+## Ícones
+- :package: Nova funcionalidade
+- :up: atualização
+- :lady_beetle: correção de bug
+- :checkered_flag: release
+
+
---
+++ js/owl/setup.js
@@ -0,0 +1,44 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+});
